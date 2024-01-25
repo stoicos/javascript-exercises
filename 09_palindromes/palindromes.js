488e4bd7de89c0b8d153894a802c96afe3876427
Stoicos <marcelo.anavia.web@gmail.com>
2024-01-24 21:03:18 -0300

    Resolve exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    if (word.split('').reverse().join('').toString().toLowerCase().replace(/[^a-z0-9]/g, "") === word.split('').join('').toString().toLowerCase().replace(/[^a-z0-9]/g, "")) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
